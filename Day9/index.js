d7748a3af30fde08d62c94faf6af93e9487d0252
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-10-08 18:23:05 +0200

    day9
    resolved

--- Day9/index.js
+++ Day9/index.js
@@ -11,10 +11,57 @@ $(function () {
     $(".random-number").click(function () {
       $(".extracted-number")(function () {
         let selected_numbers =
-          all_selected_numbers[Math.floor(Math.random() * all_numbers.length)];
+          all_selected_numbers[Math.floor(Math.floor(Math.random() * 76) + 1)];
         $(".extracted-number").text(selected_numbers);
-        console.log(selected_numbers);
+        return console.log(selected_numbers);
       });
     });
   }
 });
+
+/*
+const createCells = function(){
+
+
+const board = document.querySelector("#board");
+for (let i = 1; i <= 76; i++){
+  const Cells = document.createElement("div")
+  cell.style.width = '50px'
+  cell.style.height = '50px'
+  cell.style.backgroundColor = 'orange'
+  cell.classList.add('bingoCell')
+
+  cell.innerText = i
+
+  board.appendChild(cell)
+
+  child.ClassName = "Cells"
+
+  child.className = i + 1
+  Cell.innerText = Cell
+  child.appendChild(day)
+  board.appendChild(child)
+}
+
+}
+
+// randomize 
+const randomizeNumber = function(){
+let number Math.floor(Math.random() * 76) + 1
+console.log(number)
+let allTheCells = document.querySelectorAll('.bingoCell')
+let foundCell;
+for(let y0; y<76; y++){
+ if(allTheCells[y].innerText === number.toString()){
+   foundCell = allTheCells[y]
+ }
+}
+ 
+  console.log(foundCell)
+  // instead of for loop cells[number-1].style.backgroundcolor='yellow'
+}
+
+window.onload = createCells
+//in html
+//<input type="button" onclick="randomizeButton" />
+*/
