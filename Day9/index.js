9cf19d8c3aa2dab011d606b81af34ca5fee7a952
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-10-08 17:03:31 +0200

    day9

--- Day9/index.js
+++ Day9/index.js
@@ -3,13 +3,13 @@ $(function () {
   let all_selected_numbers = [];
 
   for (let count = 1; count < 77; count++) {
-    $("#tabella").append(
+    $("#table").append(
       `<span style="animation-delay:0.${count}s;" class="animated zoomIn"> ${count} </span>`
     );
     all_numbers.push(count);
 
-    $(".estrai-numero").click(function () {
-      $(".extracted-number").fadeOut(1000, function () {
+    $(".random-number").click(function () {
+      $(".extracted-number")(function () {
         let selected_numbers =
           all_selected_numbers[Math.floor(Math.random() * all_numbers.length)];
         $(".extracted-number").text(selected_numbers);
